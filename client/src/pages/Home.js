9b1eac169c09697b9b779e18e284851b7b92c58c
목공하는개발자 <40522161+shindalsoo@users.noreply.github.com>
2024-09-25 13:13:37 +0000

    9월25일분

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,11 +3,27 @@ import { Outlet, useLocation } from 'react-router-dom'
 import logo from '../assets/kakaotalk_logo.png'
 import Sidebar from '../components/Sidebar'
 import io from 'socket.io-client'
+import { useDispatch } from 'react-redux'
+import { setOnlineUser } from '../redux/userSlice'
+import axios from 'axios'
 
 const Home = () => {
     const location = useLocation()
     const basePath = location.pathname === '/'
+    const dispatch = useDispatch()
 
+    const fetchUserDetails = async() => {
+        const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`
+        const response = await axios({
+            url: URL,
+            withCredentials: true
+        })
+        console.log('사용자세부조회',response)
+        // 그다음 구해온 값을 리덕스에 저장
+    }
+    useEffect(()=>{
+        fetchUserDetails()
+    },[])
     useEffect(()=>{
         console.log('소켓서버랑 연결하러 왔음',localStorage.getItem('token'))
         // 소켓 서버와 연결
@@ -18,6 +34,7 @@ const Home = () => {
         })
         socketConnection.on('onlineUser',(data)=>{
             console.log(data)
+            dispatch(setOnlineUser(data))
             // 연결된 사용자들을 서버에서 줬네요. 야호 신난다....
         })
         return ()=>{
